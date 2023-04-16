c8afcbb5f83e8973dfe4822397727e88d22e4de9
hanyusok <hanyusok@gmail.com>
2023-04-16 23:27:12 +0900

    initial

---
+++ src/renderer/src/components/DataView.vue
@@ -0,0 +1,122 @@
+<script setup>
+import DataTable from 'datatables.net-vue3'
+import DataTablesCore from 'datatables.net'
+import languageKO from 'datatables.net-plugins/i18n/ko.json'
+import { ref } from 'vue'
+import { db } from '../firebase'
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
+import { format } from 'date-fns'
+import { ko } from 'date-fns/esm/locale'
+import Swal from 'sweetalert2'
+// import MessageBoard from './MessageBoard.vue'
+// import LoginView from './LoginView.vue'
+// import { useIdentityStore } from '../stores/identityStore'
+
+//login 마트의원
+// const identityStore = useIdentityStore()
+
+
+//datatable
+DataTable.use(DataTablesCore)
+
+//initialize of firestore
+const colRef = collection(db, "appointments")
+const q = query(colRef, orderBy('createdAt', 'asc'))
+const aptRef = ref([])
+let appointments = aptRef.value
+const options = {
+  responsive: true,
+  select: true,
+  language: languageKO,
+}
+
+const unsubscribe = onSnapshot(q, (snap) => {
+  snap.docChanges().forEach((change) => {
+    let changedata = change.doc.data()
+
+    //DateTime Conversion
+    let preDate = changedata.createdAt.toDate()
+    if (preDate) {
+      // changedata.createdAt = formatDistance(preDate, new Date(), { locale: ko})
+      changedata.createdAt = format(preDate, " MM월dd일p", { locale: ko })
+    }
+
+    //when new appointment add
+    changedata.id = change.doc.id
+    if (change.type === "added") {
+      appointments.unshift(changedata)
+      Swal.fire(`${changedata.name}, ${changedata.phone}, ${changedata.createdAt} 신청! `)
+    }
+    if (change.type === "modified") {
+      let index = appointments.findIndex(apt => apt.id === changedata.id)
+      Object.assign(appointments[index], changedata)
+    }
+    if (change.type === "removed") {
+      let index = appointments.findIndex(apt => apt.id === changedata.id)
+      appointments.splice(index, 1)
+    }
+  })
+},
+  (error) => {
+    console.log(error)
+  }
+)
+
+const columns = [
+  { data: 'createdAt' },
+  { data: 'name' },
+  { data: 'jumin' },
+  { data: 'history' },
+  { data: 'memo' },
+  { data: 'phone' },
+  { data: 'why' },
+  { data: 'email' },
+]
+
+</script>
+
+<template>
+  <main class="main">
+    <!-- <div v-if="!identityStore.isAllowed">
+              <login-view />
+            </div> -->
+
+    <!-- <div v-if="identityStore.isAllowed"> -->
+    <!-- <MessageBoard /> -->
+    <DataTable class="display" :columns="columns" :data="aptRef" :options="options">
+      <thead>
+        <tr>
+          <th>날짜</th>
+          <th>이름</th>
+          <th>주민번호</th>
+          <th>재진?</th>
+          <th>메모</th>
+          <th>핸드폰</th>
+          <th>내용</th>
+          <th>이메일</th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <th>날짜</th>
+          <th>이름</th>
+          <th>주민번호</th>
+          <th>재진?</th>
+          <th>메모</th>
+          <th>핸드폰</th>
+          <th>내용</th>
+          <th>이메일</th>
+        </tr>
+      </tfoot>
+    </DataTable>
+
+    <!-- </div> -->
+
+  </main>
+</template>
+<style>
+@import 'datatables.net-dt';
+@import 'datatables.net-bs5';
+@import '../../../../node_modules/sweetalert2/dist/sweetalert2.min.css'
+/* @import '../../node_modules/sweetalert2/dist/sweetalert2.min.css'; */
+</style>
---
+++ src/renderer/src/components/MessageBoard.vue
@@ -0,0 +1,192 @@
+<script setup>
+import { onMounted, ref, render } from 'vue'
+import axios from 'axios'
+import qs from 'qs'
+import { NButton, NCard, NInput, NSpace, NRadio, NTag, NSwitch, NIcon } from 'naive-ui'
+import Swal from 'sweetalert2'
+import { CommentsDollar, Tablets, Fax, ChartBarRegular, UserCheck, CommentDotsRegular } from '@vicons/fa'
+
+//kakao scripts start from here
+const kakaoLogin = () => {
+    Kakao.Auth.authorize({
+        redirectUri: 'http://127.0.0.1:5173/',
+        throughTalk: true,
+        scope: 'friends, talk_message'
+    })
+}
+
+const kakaoLogOut = () => {
+    Kakao.Auth.logout()
+        .then((resp) => {
+            Swal.fire(`로그아웃!`)
+            console.log(Kakao.Auth.getAccessToken())
+        })
+        .catch((err) => {
+            console.log('not logged in')
+        })
+}
+
+
+const authCode = location.search.substring(6)
+const access_token = ref('')
+const kakaOptions = {
+    'grant_type': 'authorization_code',
+    'client_id': '3c4ba9cc89263b9e66bca4c176a4eaf3',
+    'client_secret': '8orFiiKOUqaaP5N1fbwfARNMmIuPpJCG',
+    'code': authCode,
+    'redirect_uri': 'http://127.0.0.1:5173/'
+}
+
+onMounted(async () => {
+    await axios({
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+        },
+        data: qs.stringify(kakaOptions),
+        url: 'https://kauth.kakao.com/oauth/token'
+    })
+        .then((response) => {
+            Kakao.Auth.setAccessToken(response.data.access_token)
+            access_token.value = Kakao.Auth.getAccessToken()
+            console.log(`access_token : ${access_token.value}`)
+        })
+        .catch((err) => {
+        })
+})
+
+//수납금액
+const cost = ref()
+const shareMsgInfo = (cost) => {
+    Kakao.Share.sendCustom({
+        templateId: 85061,
+        templateArgs: {
+            COST: cost
+        }
+    });
+}
+
+//처방전 전송
+const shareMsgPharm = () => {
+    Kakao.Share.sendCustom({
+        templateId: 85349
+    });
+}
+
+//검사결과 알림
+const sharelab = () => {
+    Kakao.Share.sendCustom({
+        templateId: 92230
+    });
+}
+
+//팩스 이메일 전송
+const picked = ref("")
+const checkedValue = ref(null)
+const handleChange = (e) => {
+    checkedValue.value = e.target.value
+    picked.value = checkedValue.value
+}
+const shareFaxEmail = (picked) => {
+    Kakao.Share.sendCustom({
+        templateId: 92231,
+        templateArgs: {
+            WAY: picked
+        }
+    });
+}
+
+//메모 보내기
+const name = ref("")
+const memo = ref("")
+const shareMemo = (name, memo) => {
+    Kakao.Share.sendCustom({
+        templateId: 92233,
+        templateArgs: {
+            NAME: name,
+            MEMO: memo
+        }
+    });
+}
+
+</script>
+
+<template>
+    <div class="container">
+        <n-space justify="space-between">
+            <n-card>
+                <n-icon size="30">
+                    <user-check />
+                </n-icon>
+                카톡 사용자
+                <n-button tertiary round type="success" v-if="!access_token" @click="kakaoLogin">카카오로그인</n-button>
+                <n-button tertiary round type="error" v-if="access_token" @click="kakaoLogOut">카카오로그아웃</n-button>
+            </n-card>
+
+            <n-card>
+                <n-icon size="30">
+                    <comments-dollar />
+                </n-icon>
+                수납안내
+                <n-input round v-model:value="cost" placeholder="요청금액">
+                    <template #suffix>원</template>
+                </n-input>
+                <n-button @click="shareMsgInfo(cost)">알림톡</n-button>
+            </n-card>
+
+            <n-card>
+                <n-icon size="30">
+                    <tablets />
+                </n-icon>
+                마트약국
+                <n-button @click="shareMsgPharm">알림톡</n-button>
+            </n-card>
+
+            <n-card>
+                <n-icon size="30">
+                    <fax />
+                </n-icon>처방전 전송
+                <n-space item-style="display: flex;" align="center">
+                    <n-radio :checked="checkedValue === 'email'" value="email" @change="handleChange">이메일</n-radio>
+                    <n-radio :checked="checkedValue === 'fax'" value="fax" @change="handleChange">팩스</n-radio>
+                    <n-button @click="shareFaxEmail(picked)">알림톡</n-button>
+                </n-space>
+            </n-card>
+
+            <n-card>
+                <n-icon size="30">
+                    <chart-bar-regular />
+                </n-icon>검사결과 안내
+                <n-button @click="sharelab">알림톡</n-button>
+            </n-card>
+
+            <n-card>
+                <n-icon size="30">
+                    <comment-dots-regular />
+                </n-icon>
+                문자 전송
+                <n-space item-style="display: flex;" align="start" vertical>
+                    이름<n-input v-model:value="name" type="text" placeholder="이름" />
+                    메모<n-input maxlength="200" show-count v-model:value="memo" type="textarea" placeholder="내용" />
+                </n-space>
+                <n-button @click="shareMemo(name, memo)">알림톡</n-button>
+            </n-card>
+        </n-space>
+    </div>
+</template>
+
+<style>
+.main {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    font-weight: normal;
+
+}
+
+.n-card {
+    max-width: 200px;
+    display: inline-flex;
+}
+</style>
+
