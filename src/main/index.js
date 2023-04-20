6ee2841734ed0cc7ccec0a44587e264921f519d3
hanyusok <hanyusok@gmail.com>
2023-04-20 13:24:00 +0900

    window 2

--- src/main/index.js
+++ src/main/index.js
@@ -27,7 +27,7 @@ function createWindow() {
       action: 'allow'
     }
   })
-
+ 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
